fb48619e40a808e25a58df75fc6f2e4cf5317898
Emmanuel-9 <oregadaniel181@gmail.com>
2022-01-21 10:19:22 +0300

    Add the urls for logging in and out of the app.

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,7 +1,14 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 
 from . import views
 
 urlpatterns = [
-    path('', views.index, name="index")
+    path('', views.index, name="index"),
+    path('update/', views.update, name="update"),
+    path('add/', views.add, name="add"),
+    path('login/', views.login_user, name='login'),
+    path('logout/', views.logout, name='logout'),
+    path('register/', views.register, name='register'),
+    path('profile/', views.profile, name='profile')
 ]
