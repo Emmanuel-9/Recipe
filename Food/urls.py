0910c7d950294e91d79b8489a49eb559473ca7e3
Emmanuel-9 <oregadaniel181@gmail.com>
2022-01-21 10:23:09 +0300

    Add the fixes.

--- Food/urls.py
+++ Food/urls.py
@@ -15,10 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from recipes import views
+from recipes.views import register,login,logout
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('recipes.urls'))
+    path('', include('recipes.urls')),
+    
     
    
 ]
--- recipes/models.py
+++ recipes/models.py
@@ -1,14 +1,38 @@
-#from dis import Instruction
-#from turtle import title
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Recipe(models.Model):
     Title = models.CharField(max_length=25)
+    Image = models.ImageField(null=True)
     Description = models.CharField(max_length=150)
     Instructions = models.TextField()
     pub_date = models.DateTimeField()
+   
 
-    def __str(self):
+    def __str__(self):
         return self.Title
 
+
+    def save_recipe(self, **kwargs):
+        super().save()
+
+    def del_recipe(self):
+        self.delete()    
+
+class User(models.Model):
+    username = models.CharField(max_length=150)
+    password = models.CharField(max_length=50)
+    email = models.CharField(max_length=150)
+    first_name = models.CharField(max_length=50, blank=True)
+    last_name = models.CharField(max_length=50, blank=True)
+
+
+    def __str__(self):
+        return self.name
+
+    def save_user(self):
+        self.save()
+
+    def del_user(self):
+        self.delete()
