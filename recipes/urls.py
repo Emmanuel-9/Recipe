a803469ab8000780f6e02ba962a9d7420a2d64db
Emmanuel-9 <oregadaniel181@gmail.com>
2022-01-24 09:59:59 +0300

    Add the urls for profile page.

--- recipes/urls.py
+++ recipes/urls.py
@@ -10,5 +10,7 @@ urlpatterns = [
     path('login/', views.login_user, name='login'),
     path('logout/', views.logout, name='logout'),
     path('register/', views.register, name='register'),
-    path('profile/', views.profile, name='profile')
+    path('profile/', views.profile, name='profile'),
+    path('update_profile/', views.update_profile, name='profile_update'),
+    path('delete_recipe/<id>/', views.recipe_delete, name='recipe_delete'),
 ]
