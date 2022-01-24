58db5a404a29e5969628620eb9f5a5cc1a6c5a85
Emmanuel-9 <oregadaniel181@gmail.com>
2022-01-24 10:03:55 +0300

    Add login required restriction.

--- recipes/views.py
+++ recipes/views.py
@@ -1,11 +1,13 @@
-from pyexpat.errors import messages
+#from turtle import title
+from django.contrib import messages
 from django.http import request
-from django.shortcuts import render, redirect
-from recipes.models import Recipe,User
+from django.shortcuts import render, redirect,get_object_or_404
+from recipes.models import Recipe,Profile,User
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import login, authenticate
 from django.contrib.auth import logout as django_logout
 from recipes.forms import RecipeForm,UpdateForm,ProfileForm
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def index(request):
@@ -16,6 +18,8 @@ def index(request):
 
     return render(request,'index.html', context)
 
+
+@login_required(login_url='login')
 def add(request):
     if request.method == 'POST':
         form = RecipeForm(request.POST)
@@ -24,20 +28,21 @@ def add(request):
             return redirect('/')
     else:
         form = RecipeForm() 
-    return render (request,'user/add_recipe.html',{"form": form})
+    return render(request,'user/add_recipe.html',{"form": form})
 
 
 
-
-def update(request):
+@login_required(login_url='login')
+def update(request, username):
+    user = User.objects.get(username=username)
     if request.method == 'POST':
-        form = UpdateForm(request.POST)
+        form = UpdateForm(request.POST, request.FILES, instance=request.user.id)
         if form.is_valid():
             form.save()
             return redirect('/')
     else:
-        form = UpdateForm() 
-    return render (request,'user/update.html',{"form": form})
+        form = UpdateForm(request.POST, request.FILES, instance=request.user.id) 
+    return render(request,'user/update.html',{"form": form})
 
 def register(request):
     if request.method == 'POST':
@@ -48,9 +53,9 @@ def register(request):
             password1 = form.cleaned_data['password1']
             password2 = form.cleaned_data['password2']
             user = authenticate(username=username, password1=password1, password2=password2)
-            login(request, user)
-            messages.success(request, ("Registration Successful"))
-            return redirect('/')
+            #login(request, user)
+            messages.success(request, "Registration Successful")
+            return redirect('login')
     else:
         form = UserCreationForm()
 
@@ -73,16 +78,35 @@ def login_user(request):
 
 def logout(request):
     django_logout(request)
-    #messages.success(request, ("You were logged out"))
+    messages.success(request, ("You were logged out"))
     return redirect('/')         
 
 
 def profile(request):
+    profile_list = Profile.objects.all()
+    context = {
+        'profile_list': profile_list,
+    }
+
+    return render(request,'user/profile.html', context)
+
+ 
+@login_required(login_url='login')
+def update_profile(request, username):
+    user = User.objects.get(username=username)
     if request.method == 'POST':
-        form = ProfileForm(request.POST)
+        form = ProfileForm(request.POST,request.FILES, instance=request.user.userprofile)
         if form.is_valid():
             form.save()
-            return redirect('/')
+            return redirect('profile', user.id)
     else:
-        form = ProfileForm() 
-    return render (request,'user/profile.html',{"form": form})    
+        form = ProfileForm(request.POST,request.FILES, instance=request.user.userprofile) 
+    return render(request,'user/update_profile.html',{"form": form})  
+
+
+@login_required(login_url='login')
+def recipe_delete(request, id):
+    recipe_be_deleted = get_object_or_404(Recipe, id=id)
+    if request.method == 'POST':
+        recipe_be_deleted.delete() 
+        return redirect('/')   
